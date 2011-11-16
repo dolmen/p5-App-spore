c047290d93da5fad02c13182c6275698e12b08bb
Olivier Mengué <dolmen@cpan.org>
2011-11-17 00:23:08 +0100

    Initial commit, with a working version
    Works with my SPORE spec for the Vélib' service.

---
+++ lib/App/spore.pm
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+package App::spore;
+
+1;
+__END__
+
+=head1 NAME
+
+App::spore - A command-line client for web services defined with SPORE specs
+
+=head1 DESCRIPTION
+
+See L<spore>
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+L<https://github.com/SPORE> : SPORE specifications and some APIs descriptions
+
+=item *
+
+L<Net::HTTP::Spore> : an implementation of SPORE for Perl
+
+=back
+
+=head1 AUTHOR
+
+Olivier MenguE<eacute>, L<mailto:dolmen@cpan.org>.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright E<copy> 2011 Olivier MenguE<eacute>.
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl 5 itself.
+
+=cut
+
